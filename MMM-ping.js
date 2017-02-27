99ab42b4d9ca83b08efaff013f7f4f8785473d76
fewieden <fewieden@htwg-konstanz.de>
2017-02-27 13:43:31 +0100

    add linter

--- MMM-ping.js
+++ MMM-ping.js
@@ -1,4 +1,14 @@
-Module.register("MMM-ping",{
+/* global Module Log */
+
+/* Magic Mirror
+ * Module: MMM-ping
+ *
+ * By fewieden https://github.com/fewieden/MMM-ping
+ *
+ * MIT Licensed.
+ */
+
+Module.register('MMM-ping', {
     defaults: {
         colored: false,
         display: 'both',
@@ -7,37 +17,36 @@ Module.register("MMM-ping",{
         font: 'medium'
     },
 
-    start: function() {
-        Log.info("Starting module: " + this.name);
+    start() {
+        Log.info(`Starting module: ${this.name}`);
         this.status = {};
         this.checkHosts();
         setInterval(() => {
             this.checkHosts();
-        }, this.config.updateInterval*60000);
+        }, this.config.updateInterval * 60000);
     },
 
-    checkHosts: function(){
+    checkHosts() {
         this.sendSocketNotification('CHECK_HOSTS', this.config.hosts);
     },
 
-    getDom: function() {
-        var wrapper = document.createElement("div");
+    getDom() {
+        const wrapper = document.createElement('div');
         wrapper.classList.add(this.config.font);
         wrapper.style.textAlign = 'left';
-        var hosts = Object.keys(this.status);
-        if(hosts.length > 0){
-            for(var i = 0; i < hosts.length; i++){
-                var isOnline = this.status[hosts[i]];
-                if(isOnline && (this.config.display === 'both' || this.config.display === 'online') ||
-                    !isOnline && (this.config.display === 'both' || this.config.display === 'offline')){
-
-                    var div = document.createElement("div");
-                    var span = document.createElement("span");
+        const hosts = Object.keys(this.status);
+        if (hosts.length > 0) {
+            for (let i = 0; i < hosts.length; i += 1) {
+                const isOnline = this.status[hosts[i]];
+                if ((isOnline && (this.config.display === 'both' || this.config.display === 'online')) ||
+                    (!isOnline && (this.config.display === 'both' || this.config.display === 'offline'))) {
+                    const div = document.createElement('div');
+                    const span = document.createElement('span');
                     span.innerHTML = isOnline ? '&#9899;' : '&#9898;';
-                    if(this.config.colored){
+                    if (this.config.colored) {
                         span.style.color = isOnline ? 'green' : 'red';
                     }
-                    var host = document.createElement("span");
+                    const host = document.createElement('span');
                     host.innerHTML = hosts[i];
                     div.appendChild(span);
                     div.appendChild(host);
@@ -48,8 +57,8 @@ Module.register("MMM-ping",{
         return wrapper;
     },
 
-    socketNotificationReceived: function(notification, payload) {
-        if (notification === "HOST"){
+    socketNotificationReceived(notification, payload) {
+        if (notification === 'HOST') {
             this.status[payload.host] = payload.status;
             this.updateDom();
         }
