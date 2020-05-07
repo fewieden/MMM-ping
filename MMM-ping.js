ecf6ed2c8a1f9d870a8f59f07b4f67fa04b81c47
Felix Wiedenbach <felix.wiedenbach@fleetster.net>
2020-05-07 21:28:20 +0200

    module rewrite

--- MMM-ping.js
+++ MMM-ping.js
@@ -1,14 +1,49 @@
-/* global Module Log */
-
-/* Magic Mirror
- * Module: MMM-ping
+/**
+ * @file MMM-ping.js
  *
- * By fewieden https://github.com/fewieden/MMM-ping
+ * @author fewieden
+ * @license MIT
  *
- * MIT Licensed.
+ * @see https://github.com/fewieden/MMM-ping
  */
 
+/* global Module Log */
+
+/**
+ * @external Module
+ * @see https://github.com/MichMich/MagicMirror/blob/master/js/module.js
+ */
+
+/**
+ * @external Log
+ * @see https://github.com/MichMich/MagicMirror/blob/master/js/logger.js
+ */
+
+/**
+ * @module MMM-ping
+ * @description Frontend for the module to display data.
+ *
+ * @requires external:Module
+ * @requires external:Log
+ */
 Module.register('MMM-ping', {
+    /** @member {Object} status - List of results with hosts and their online status. */
+    status: [],
+
+    /** @member {Object} onlineMapping - Map online states to strings. */
+    onlineMapping: {
+        true: 'online',
+        false: 'offline'
+    },
+
+    /**
+     * @member {Object} defaults - Defines the default config values.
+     * @property {boolean} colored - Flag to render map in color or greyscale.
+     * @property {string} display - Online states which should be displayed.
+     * @property {string[]} hosts - List of hosts to ping.
+     * @property {int} updateInterval - Speed of update.
+     * @property {string} font - Class name for font size.
+     */
     defaults: {
         colored: false,
         display: 'both',
@@ -17,50 +52,169 @@ Module.register('MMM-ping', {
         font: 'medium'
     },
 
+    /**
+     * @member {Object} voice - Defines the voice recognition part.
+     * @property {string} mode - MMM-voice mode of this module.
+     * @property {string[]} sentences - All commands of this module.
+     */
+    voice: {
+        mode: 'PING',
+        sentences: [
+            'OPEN HELP',
+            'CLOSE HELP',
+            'SHOW ALL Hosts',
+            'HIDE HOSTS'
+        ]
+    },
+
+    /**
+     * @function getTranslations
+     * @description Translations for this module.
+     * @override
+     *
+     * @returns {Object.<string, string>} Available translations for this module (key: language code, value: filepath).
+     */
+    getTranslations() {
+        return {
+            en: 'translations/en.json',
+            de: 'translations/de.json'
+        };
+    },
+
+    /**
+     * @function getStyles
+     * @description Style dependencies for this module.
+     * @override
+     *
+     * @returns {string[]} List of the style dependency filepaths.
+     */
+    getStyles() {
+        return ['MMM-ping.css'];
+    },
+
+    /**
+     * @function getTemplate
+     * @description Nunjuck template.
+     * @override
+     *
+     * @returns {string} Path to nunjuck template.
+     */
+    getTemplate() {
+        return 'templates/MMM-ping.njk';
+    },
+
+    /**
+     * @function getTemplateData
+     * @description Data that gets rendered in the nunjuck template.
+     * @override
+     *
+     * @returns {object} Data for the nunjuck template.
+     */
+    getTemplateData() {
+        const status = this.status.filter(entry => this.config.display === 'both' || this.config.display === this.onlineMapping[entry.online]);
+
+        return {
+            config: this.config,
+            status
+        };
+    },
+
+    /**
+     * @function start
+     * @description Initiates ping interval.
+     * @override
+     *
+     * @returns {void}
+     */
     start() {
         Log.info(`Starting module: ${this.name}`);
-        this.status = {};
         this.checkHosts();
         setInterval(() => {
             this.checkHosts();
         }, this.config.updateInterval * 60000);
     },
 
+    /**
+     * @function checkHosts
+     * @description Sends a notification to node_helper to ping hosts again.
+     * @override
+     *
+     * @returns {void}
+     */
     checkHosts() {
         this.sendSocketNotification('CHECK_HOSTS', this.config.hosts);
     },
 
-    getDom() {
-        const wrapper = document.createElement('div');
-        wrapper.classList.add(this.config.font);
-        wrapper.style.textAlign = 'left';
-        const hosts = Object.keys(this.status);
-        if (hosts.length > 0) {
-            for (let i = 0; i < hosts.length; i += 1) {
-                const isOnline = this.status[hosts[i]];
-                if ((isOnline && (this.config.display === 'both' || this.config.display === 'online')) ||
-                    (!isOnline && (this.config.display === 'both' || this.config.display === 'offline'))) {
-                    const div = document.createElement('div');
-                    const span = document.createElement('span');
-                    span.innerHTML = isOnline ? '&#9899;' : '&#9898;';
-                    if (this.config.colored) {
-                        span.style.color = isOnline ? 'green' : 'red';
-                    }
-                    const host = document.createElement('span');
-                    host.innerHTML = hosts[i];
-                    div.appendChild(span);
-                    div.appendChild(host);
-                    wrapper.appendChild(div);
-                }
-            }
+    /**
+     * @function socketNotificationReceived
+     * @description Handles incoming messages from node_helper.
+     * @override
+     *
+     * @param {string} notification - Notification name
+     * @param {*} payload - Detailed payload of the notification.
+     */
+    socketNotificationReceived(notification, payload) {
+        if (notification === 'STATUS_UPDATE') {
+            this.status = payload;
+            this.updateDom(300);
         }
-        return wrapper;
     },
 
-    socketNotificationReceived(notification, payload) {
-        if (notification === 'HOST') {
-            this.status[payload.host] = payload.status;
-            this.updateDom();
+    /**
+     * @function notificationReceived
+     * @description Handles incoming broadcasts from other modules or the MagicMirror core.
+     * @override
+     *
+     * @param {string} notification - Notification name
+     * @param {*} payload - Detailed payload of the notification.
+     * @param {MM} [sender] - The sender of the notification. If sender is undefined the sender is the core.
+     */
+    notificationReceived(notification, payload, sender) {
+        if (notification === 'ALL_MODULES_STARTED') {
+            this.sendNotification('REGISTER_VOICE_MODULE', this.voice);
+        } else if (notification === 'VOICE_PING' && sender.name === 'MMM-voice') {
+            this.checkCommands(payload);
+        } else if (notification === 'VOICE_MODE_CHANGED' && sender.name === 'MMM-voice' && payload.old === this.voice.mode) {
+            this.sendNotification('CLOSE_MODAL');
+        }
+    },
+
+    /**
+     * @function checkCommands
+     * @description Checks for voice commands.
+     *
+     * @param {string} data - Text with commands.
+     *
+     * @returns {void}
+     */
+    checkCommands(data) {
+        if (/(HELP)/g.test(data)) {
+            if (/(CLOSE)/g.test(data) && !/(OPEN)/g.test(data)) {
+                this.sendNotification('CLOSE_MODAL');
+            } else if (/(OPEN)/g.test(data) && !/(CLOSE)/g.test(data)) {
+                this.sendNotification('OPEN_MODAL', {
+                    template: 'templates/HelpModal.njk',
+                    data: {
+                        ...this.voice,
+                        fns: {
+                            translate: this.translate.bind(this)
+                        }
+                    }
+                });
+            }
+        } else if (/(HIDE)/g.test(data) && /(HOSTS)/g.test(data)) {
+            this.sendNotification('CLOSE_MODAL');
+        } else if (/(SHOW)/g.test(data) && /(HOSTS)/g.test(data)) {
+            this.sendNotification('OPEN_MODAL', {
+                template: 'templates/PingModal.njk',
+                data: {
+                    config: this.config,
+                    status: this.status,
+                    fns: {
+                        translate: this.translate.bind(this)
+                    }
+                }
+            });
         }
     }
 });
--- node_helper.js
+++ node_helper.js
@@ -1,24 +1,54 @@
-/* Magic Mirror
- * Module: MMM-ping
+/**
+ * @file node_helper.js
  *
- * By fewieden https://github.com/fewieden/MMM-ping
+ * @author fewieden
+ * @license MIT
  *
- * MIT Licensed.
+ * @see  https://github.com/fewieden/MMM-ping
  */
 
-/* eslint-env node */
-
+/**
+ * @external node_helper
+ * @see https://github.com/MichMich/MagicMirror/blob/master/modules/node_modules/node_helper/index.js
+ */
 const NodeHelper = require('node_helper');
+
+/**
+ * @external ping
+ * @see https://www.npmjs.com/package/ping
+ */
 const ping = require('ping');
 
+/**
+ * @module node_helper
+ * @description Backend for the module to ping hosts.
+ *
+ * @requires external:ping
+ * @requires external:node_helper
+ */
 module.exports = NodeHelper.create({
-    socketNotificationReceived(notification, payload) {
+    /**
+     * @function socketNotificationReceived
+     * @description Receives socket notifications from the module.
+     * @async
+     * @override
+     *
+     * @param {string} notification - Notification name
+     * @param {*} payload - Detailed payload of the notification.
+     *
+     * @returns {void}
+     */
+    async socketNotificationReceived(notification, payload) {
         if (notification === 'CHECK_HOSTS') {
-            payload.forEach((host) => {
-                ping.sys.probe(host, (isAlive) => {
-                    this.sendSocketNotification('HOST', { host, status: isAlive });
-                });
-            });
+            const status = [];
+
+            for (const host of payload) {
+                const { alive } = await ping.promise.probe(host);
+
+                status.push({ host, online: alive });
+            }
+
+            this.sendSocketNotification('STATUS_UPDATE', status);
         }
     }
 });
